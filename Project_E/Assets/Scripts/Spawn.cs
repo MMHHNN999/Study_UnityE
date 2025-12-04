8b22d7949ca63dffed7e5e9fb8127169b1dc8ce6
MMHHNN999 <1raina7@gmail.com>
2025-12-04 14:37:08 +0900

    [Add]Study08_에셋
    로딩~에셋넣는법

---
+++ Project_E/Assets/Scripts/Spawn.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+
+
+public class Spawn : MonoBehaviour
+{
+    public GameObject targetPrefab;
+
+    public void SpawnTarget()
+    {
+        Instantiate(targetPrefab, transform.position, Quaternion.identity);/*기준각도*/
+    }
+
+    
+ 
+    
+}
